edb52aed65dbd10cd747204fb824a6ca7b1079d2
Wietse Neven <info@wn-multimedia.nl>
2016-10-03 21:26:11 +0200

    SCSS parsing

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
 const app = express();
 
+// Autocompile scss files to css and make them public
+app.use(sassMiddleware({
+  /* Options */
+  src: path.join(__dirname, 'src/scss'),
+  dest: path.join(__dirname, 'public/assets/styles'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix:  '/assets/styles'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
+
+// Make everything in the public directory public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use pug/jade as the render view engine
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
