202be17017fd262d8adbaaf5dadb4d353b807e8f
Wietse Neven <info@wn-multimedia.nl>
2016-10-03 22:01:34 +0200

    Switched to gulp css transpiling for better browsersync adventages

---
+++ Gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync');
+const nodemon = require('gulp-nodemon');
+
+
+gulp.task('default', ['sass', 'watch', 'browser-sync']);
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('public/assets/styles'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', () => {
+  // gulp.watch('./src/js/**/*.*', ['scripts']);
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('browser-sync', ['nodemon'], function () {
+  browserSync.init(null, {
+    proxy: "http://localhost:7933",
+    files: ["public/**/*.*"],
+    browser: "google chrome",
+    port: 3000,
+  });
+});
+gulp.task('nodemon', function (cb) {
+
+  var started = false;
+
+  return nodemon({
+    script: 'app.js'
+  }).on('start', function () {
+    // to avoid nodemon being started multiple times
+    // thanks @matthisk
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
--- app.js
+++ app.js
@@ -1,17 +1,7 @@
+'use strict';
 const express = require('express');
-const sassMiddleware = require('node-sass-middleware');
 const path = require('path');
 const app = express();
-
-// Autocompile scss files to css and make them public
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'src/scss'),
-  dest: path.join(__dirname, 'public/assets/styles'),
-  debug: false,
-  outputStyle: 'compressed',
-  prefix:  '/assets/styles',
-  sourceMap: __dirname + '/public/maps/sourcemaps.maps'
-}));
 
 // Make everything in the public directory public
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,9 +10,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  //res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.sendfile('./public/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(7933, function () {
+  console.log('Example app listening on port 7933!');
 });
